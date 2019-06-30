b83608caed4754bca7b3c406070e347f500ab339
Elton Stoneman <elton@sixeyed.com>
2019-06-30 22:53:14 +0100

    Ch04 web app in progress

---
+++ ch04/exercises/image-gallery/src/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace src.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string ImageUrl {get; private set;}
+        public string Caption {get; private set;}
+        public string Copyright {get; private set;}
+
+        public void OnGet()
+        {
+            ImageUrl = "https://www.youtube.com/embed/YYxPw_T8Vlk";
+            Caption ="Virtual Flight over Asteroid Vesta";
+        }
+    }
+}
