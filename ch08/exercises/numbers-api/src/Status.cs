5367aaee50f74a193fc9bca4a271b85e663231f0
Elton Stoneman <elton@sixeyed.com>
2019-10-09 21:32:07 +0100

    Ch08 - API with healthcheck

---
+++ ch08/exercises/numbers-api/src/Status.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Numbers.Api
+{
+    public static class Status
+    {
+        public static bool Healthy { get; set; }
+
+        static Status()
+        {
+            Healthy = true;
+        }
+    }
+}
