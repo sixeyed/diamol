2ade5f1965120212b2a41d61256b4baf35e406de
Elton Stoneman <elton@sixeyed.com>
2019-11-08 10:42:10 +0000

    Add Jenkins for Linux

--- images/jenkins/linux/scripts/install-plugins.groovy
+++ images/jenkins/linux/scripts/install-plugins.groovy
@@ -1,9 +1,13 @@
+#!groovy
+
 import jenkins.model.Jenkins;
 
 pm = Jenkins.instance.pluginManager
 uc = Jenkins.instance.updateCenter
 
-["git", "credentials", "workflow-aggregator", "cloudbees-folder"].each {
+pm.doCheckUpdatesServer()
+
+["git", "workflow-aggregator"].each {
     if (! pm.getPlugin(it)) {
     deployment = uc.getPlugin(it).deploy(true)
     deployment.get()
