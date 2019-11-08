59113342d4375464feb820831e62172488aaac1c
Elton Stoneman <elton@sixeyed.com>
2019-11-08 13:17:20 +0000

    Add Windows Jenkins

---
+++ images/jenkins/windows/scripts/install-plugins.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+import jenkins.model.Jenkins;
+
+pm = Jenkins.instance.pluginManager
+uc = Jenkins.instance.updateCenter
+
+pm.doCheckUpdatesServer()
+
+["git", "workflow-aggregator"].each {
+    if (! pm.getPlugin(it)) {
+    deployment = uc.getPlugin(it).deploy(true)
+    deployment.get()
+    }
+}
+
+// Jenkins.instance.restart()
