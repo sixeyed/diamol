13a3dace3d2502049fe4b0c08bf8f1c5d763727f
Elton Stoneman <elton@sixeyed.com>
2019-11-29 11:41:06 +0000

    Debug build

--- ch15/exercises/ci/04-smoke.bat
+++ ch15/exercises/ci/04-smoke.bat
@@ -1,2 +1,6 @@
-echo "Deploying to $SMOKE_ENGINE; with CA: $ca, cert: $cert; key: $key"
-docker-compose --host "tcp://$SMOKE_ENGINE" --tlsverify --tlscacert $ca --tlscert $cert --tlskey $key -f docker-compose.yml -f docker-compose-smoketest.yml up -d
+echo "Deploying to $SMOKE_ENGINE"
+stat $ca
+stat $cert
+stat $key
+docker-compose -f docker-compose.yml -f docker-compose-smoketest.yml config
+docker-compose --host tcp://$SMOKE_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $key -f docker-compose.yml -f docker-compose-smoketest.yml up -d
