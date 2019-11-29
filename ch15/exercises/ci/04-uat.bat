d9e82053f9932f3be772aae72b09d1a614db962f
Elton Stoneman <elton@sixeyed.com>
2019-11-29 12:51:10 +0000

    Fix build?

--- ch15/exercises/ci/04-uat.bat
+++ ch15/exercises/ci/04-uat.bat
@@ -1,2 +1,2 @@
 echo "Deploying to $UAT_ENGINE"
-docker-compose --host tcp://$UAT_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $ke -p timecheck-uat -f docker-compose.yml -f docker-compose-uat.yml up -d
+docker-compose --host tcp://$UAT_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $ke -p timecheck_uat -f docker-compose.yml -f docker-compose-uat.yml up -d
