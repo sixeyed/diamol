14391d4d4bb202f634a5d0763fef50c67768b7c1
Elton Stoneman <elton@sixeyed.com>
2019-11-29 20:33:33 +0000

    Fix variable names

--- ch15/lab/ci/03-push.bat
+++ ch15/lab/ci/03-push.bat
@@ -2,6 +2,6 @@ docker-compose -f docker-compose.yml -f docker-compose-build.yml push
 
 docker image tag $REGISTRY/diamol/ch15-timecheck:3.0-build-$BUILD_NUMBER sixeyed/diamol-ch15-timecheck:3.0
 
-docker login -u $username -p $password
+docker login -u $hub_user -p $hub_password
 
 docker image push sixeyed/diamol-ch15-timecheck:3.0
