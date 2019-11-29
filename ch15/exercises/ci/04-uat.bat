f1e380dfe7e0f07062d8991ab6f3c35bb77d16c4
Elton Stoneman <elton@sixeyed.com>
2019-11-29 12:49:35 +0000

    Fix build

--- ch15/exercises/ci/04-uat.bat
+++ ch15/exercises/ci/04-uat.bat
@@ -1,3 +1,2 @@
 echo "Deploying to $UAT_ENGINE"
-docker-compose -f docker-compose.yml -f docker-compose-uat.yml config
-docker-compose --host tcp://$UAT_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $key -f docker-compose.yml -f docker-compose-uat.yml up -d
+docker-compose --host tcp://$UAT_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $ke -p timecheck-uat -f docker-compose.yml -f docker-compose-uat.yml up -d
--- ch15/exercises/ci/05-deploy.bat
+++ ch15/exercises/ci/05-deploy.bat
@@ -1,1 +1,2 @@
-docker-compose --host tcp://$PROD_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $key -p timecheck-pro -f docker-compose.yml -f docker-compose-prod.yml up -d
+echo "Deploying to $PROD_ENGINE"
+docker-compose --host tcp://$PROD_ENGINE --tlsverify --tlscacert $ca --tlscert $cert --tlskey $key -p timecheck-prod -f docker-compose.yml -f docker-compose-prod.yml up -d
