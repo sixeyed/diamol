a8f1effbbac294c749c5614b69fceec7533e6945
Elton Stoneman <elton@sixeyed.com>
2020-02-04 09:19:49 +0000

    Add home page to Java apps

--- ch04/exercises/image-of-the-day/src/main/java/iotd/Application.java
+++ ch04/exercises/image-of-the-day/src/main/java/iotd/Application.java
@@ -2,9 +2,17 @@ package iotd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Nothing to see here, try /image";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- ch17/exercises/truth-app/src/main/java/truth/Application.java
+++ ch17/exercises/truth-app/src/main/java/truth/Application.java
@@ -1,10 +1,18 @@
-package truth;
+package iotd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Nothing to see here, try /truth";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
