221ee4c9e1fbdaeda5e697ac413db3ea29c21783
Elton Stoneman <elton@sixeyed.com>
2020-02-04 08:43:01 +0000

    Listen for exit signal

--- ch03/exercises/web-ping/app.js
+++ ch03/exercises/web-ping/app.js
@@ -1,24 +1,38 @@
-const https = require('https');
+const https = require("https");
 
 const options = {
-    hostname: process.env.TARGET,
-    method: process.env.METHOD
-  };
+  hostname: process.env.TARGET,
+  method: process.env.METHOD
+};
 
-console.log('** web-ping ** Pinging: %s; method: %s; %dms intervals', options.hostname, options.method, process.env.INTERVAL);
-  
+console.log(
+  "** web-ping ** Pinging: %s; method: %s; %dms intervals",
+  options.hostname,
+  options.method,
+  process.env.INTERVAL
+);
+
+process.on("SIGINT", function() {
+  process.exit();
+});
+
 let i = 1;
 let start = new Date().getTime();
-setInterval(() => {    
-    start = new Date().getTime();
-    console.log('Making request number: %d; at %d', i++, start);
-    var req = https.request(options, (res) => {
-        var end = new Date().getTime();    
-        var duration = end-start;    
-        console.log('Got response status: %s at %d; duration: %dms', res.statusCode, end, duration);
-    });
-    req.on('error', (e) => {
-        console.error(e);
-      });
-    req.end();
-}, process.env.INTERVAL)
+setInterval(() => {
+  start = new Date().getTime();
+  console.log("Making request number: %d; at %d", i++, start);
+  var req = https.request(options, res => {
+    var end = new Date().getTime();
+    var duration = end - start;
+    console.log(
+      "Got response status: %s at %d; duration: %dms",
+      res.statusCode,
+      end,
+      duration
+    );
+  });
+  req.on("error", e => {
+    console.error(e);
+  });
+  req.end();
+}, process.env.INTERVAL);
