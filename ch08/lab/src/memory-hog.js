cc762975055d84411dc08662b9f54382108c2958
Elton Stoneman <elton@sixeyed.com>
2024-08-28 17:00:27 +0100

    Ch08 builds for Linux/arm64

--- ch08/lab/src/memory-hog.js
+++ ch08/lab/src/memory-hog.js
@@ -12,7 +12,7 @@ function allocate() {
 
   var allocatedMb = process.env.LOOP_ALLOCATION_MB * loop;
   console.log(`Allocated: ${allocatedMb}MB`);
-  fs.writeFileSync("ALLOCATED_MB", allocatedMb, "utf-8");
+  fs.writeFileSync("ALLOCATED_MB", allocatedMb.toString(), "utf-8");
 
   loop++;
   setTimeout(allocate, process.env.LOOP_INTERVAL_MS);
